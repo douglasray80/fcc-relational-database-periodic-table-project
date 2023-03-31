b1fd6bf31ba73ee81ca62be36441093c11d8a33f
douglasray80 <dpr980@gmail.com>
2023-03-31 13:11:51 +0000

    feat: print search result

--- element.sh
+++ element.sh
@@ -13,18 +13,20 @@ elif [[ $1 =~ ^[1-9]$|^10$ ]]
 then
   # if argument is a number between 1 - 10
   echo "Argument provided is valid"
-  ELEMENT_RESULT=$($PSQL "SELECT * FROM elements FULL JOIN properties USING(atomic_number) WHERE atomic_number = $1")
+  ELEMENT_RESULT=$($PSQL "SELECT * FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE atomic_number = $1")
 elif [[ $1 =~ ^[A-Z][a-z]?$ ]]
 then
   # if argument is two letters starting with an uppercase and ending with a lowercase 
-  ELEMENT_RESULT=$($PSQL "SELECT * FROM elements FULL JOIN properties USING(atomic_number) WHERE symbol = '$1'")
+  ELEMENT_RESULT=$($PSQL "SELECT * FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE symbol = '$1'")
 else
-  ELEMENT_RESULT=$($PSQL "SELECT * FROM elements FULL JOIN properties USING(atomic_number) WHERE name = '$1'")
+  # query the db using element name
+  ELEMENT_RESULT=$($PSQL "SELECT * FROM elements FULL JOIN properties USING(atomic_number) FULL JOIN types USING(type_id) WHERE name = '$1'")
 fi
 
+# if there is no valid result
 if [[ -z $ELEMENT_RESULT ]]
 then
   echo "I could not find that element in the database."
 else
-  echo "$ELEMENT_RESULT"
+  echo $ELEMENT_RESULT
 fi
