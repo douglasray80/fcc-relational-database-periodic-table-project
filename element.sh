84fa082db78b455de565c9a378422bf3d9af297d
douglasray80 <dpr980@gmail.com>
2023-03-31 11:02:07 +0000

    feat: exit with message if no args provided

--- element.sh
+++ element.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # Program that provides information about any element in the periodic table based on user input.
+
+if [[ -z $1 ]]
+then
+  # print message and exit if no arguments provided.
+  echo "Please provide an element as an argument."
+fi
