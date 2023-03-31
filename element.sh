46f9edd13f04c5936da0821b7c9a0ab9d670ac92
douglasray80 <dpr980@gmail.com>
2023-03-31 12:57:39 +0000

    feat: query database by element symbol

--- element.sh
+++ element.sh
@@ -13,6 +13,10 @@ then
   # if argument is a number between 1 - 10
   echo "Argument provided is valid"
   ELEMENT_RESULT=$($PSQL "SELECT * FROM elements FULL JOIN properties USING(atomic_number) WHERE atomic_number = $1")
+elif [[ $1 =~ ^[A-Z][a-z]?$ ]]
+then
+  # if argument is two letters starting with an uppercase and ending with a lowercase 
+  ELEMENT_RESULT=$($PSQL "SELECT * FROM elements FULL JOIN properties USING(atomic_number) WHERE symbol = '$1'")
 else
   echo "Invalid argument"
 fi
